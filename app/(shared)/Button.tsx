a841973c4dfd05a99a813a1c50bf2ba64d9ea43e
Tais Souza <tais.fernandes.ssouza@gmail.com>
2025-07-26 00:56:48 -0300

    Started button component

---
+++ app/(shared)/Button.tsx
@@ -0,0 +1,77 @@
+"use client";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+
+export default function Button({
+  content,
+  onClick,
+  rippleColor,
+  outlined,
+  mainColor = "var(--primary)",
+  ripple = false,
+}: {
+  content: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  rippleColor?: string;
+  outlined?: boolean;
+  mainColor?: string;
+  ripple?: boolean;
+}) {
+  const [isRipple, setIsRipple] = useState(false);
+  const [coords, setCoords] = useState({ x: -1, y: -1 });
+
+  useEffect(() => {
+    if (coords.x !== -1 && coords.y !== -1) {
+      setIsRipple(true);
+
+      setTimeout(() => setIsRipple(false), 180);
+    } else {
+      setIsRipple(false);
+    }
+  }, [coords]);
+
+  useEffect(() => {
+    if (!isRipple) setCoords({ x: -1, y: -1 });
+  }, [isRipple]);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+
+    setCoords({
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    });
+
+    onClick?.(e);
+  };
+
+  console.log(isRipple);
+  return (
+    <button
+      onClick={handleClick}
+      className={classNames("button", { "button--outlined": outlined })}
+      style={
+        outlined
+          ? { color: mainColor }
+          : ({
+              backgroundColor: mainColor,
+              "--main-color": mainColor,
+            } as React.CSSProperties)
+      }
+    >
+      {ripple && isRipple ? (
+        <span
+          className="button--ripple"
+          style={{
+            left: coords.x,
+            top: coords.y,
+            backgroundColor: rippleColor,
+          }}
+        ></span>
+      ) : (
+        ""
+      )}
+      <span className="content">{content}</span>
+    </button>
+  );
+}
